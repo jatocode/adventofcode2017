56f31edecc12245bb5114b774d51ef950f8f1b01
Tobias Jansson <tobias.jansson@gmail.com>
2017-12-02 10:28:28 +0100

    Preparing for tomorrow

---
+++ 3.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+var args = process.argv.slice(2);
+
+function read(file, callback) {
+    fs.readFile(file, 'utf8', function(err, data) {
+        if (err) {
+            console.log(err);
+        }
+        callback(data);
+    });
+}
+
+read(args[0], function(data) {
+    var lines = data.split('\n');
+
+    for(let l of lines) {
+        if(l.length == 0) continue;
+        var nums = l.split(/\s+/).map((a) => parseInt(a));
+        
+    }
+
+});
+
