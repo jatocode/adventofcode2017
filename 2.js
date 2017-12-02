08f9cbb59bc7dc2c505c84e737317e3097538209
Tobias Jansson <tobias.jansson@gmail.com>
2017-12-02 09:16:13 +0100

    Part 2

--- 2.js
+++ 2.js
@@ -13,6 +13,7 @@ function read(file, callback) {
 read(args[0], function(data) {
     var lines = data.split('\n');
     var sum = 0;
+    var sum2 = 0;
     for(let l of lines) {
         if(l.length == 0) continue;
         var nums = l.split(/\s+/).map((a) => parseInt(a));
@@ -23,7 +24,18 @@ read(args[0], function(data) {
             currmin = Math.min(num, currmin);
         }
         sum = sum + (currmax - currmin);
+
+        var divisible;
+        for(num of nums) {
+            for(num2 of nums) {
+                if(num != num2 && num % num2 == 0)  {
+                    divisible = [num, num2];
+                }
+            }
+        }
+        sum2 = sum2 + divisible[0] / divisible[1];
     }
-    console.log('Part 1: ' + sum);
+    console.log("Part 1: " + sum);
+    console.log("Part 2: " + sum2);
 });
 
