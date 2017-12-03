acaaaca26cd83adcffae2f7f05ad3b9e717b4ee8
Tobias Jansson <tobias.jansson@gmail.com>
2017-12-03 19:32:18 +0100

    Ugly but working part 1

--- 3.js
+++ 3.js
@@ -1,23 +1,57 @@
 var fs = require('fs');
 var args = process.argv.slice(2);
 
-function read(file, callback) {
-    fs.readFile(file, 'utf8', function(err, data) {
-        if (err) {
-            console.log(err);
-        }
-        callback(data);
-    });
+/*              31 30
+17  16  15  14  13 30
+18   5   4   3  12 29
+19   6   1   2  11 28
+20   7   8   9  10 27
+21  22  23  24  25 26
+*/
+
+const input = 265149;
+
+var spiral = [];
+var xmax = 1;
+var xmin = -1;
+var ymax = 1;
+var ymin = -1;
+var x = 0;
+var y = 0;
+
+for(var i=1; i<=input;i++) {
+    spiral.push({sq:i,x:x,y:y});
+    d = direction(x,y);
+    x += d[0];
+    y += d[1];
+    if(x == xmax && y == ymin) {
+        xmax++;
+        xmin--;
+        ymin--;
+        ymax++;
+    }
+}
+console.log('Its ' + manh(spiral[0], spiral[input-1]) + ' steps to ' + input);
+
+function direction(x,y) {
+    var dx = 0;
+    if(x == xmax) {
+        if(y == ymin)  return [1,0]; // Next spiral 9 -> 10
+        if(y == ymax) return [-1,0]; // Go left 3 -> 4
+        return [0,1]; // Go upp 2 -> 3
+    } 
+    if(x == xmin) {
+        if(y == ymin)  return [1,0]; // Go right 7 - 8
+        if(y == ymax) return [0,-1]; // 5 - > 6
+        return [0,-1]; // Go down // 6 -> 7
+    }
+    if(y == ymin) return [1,0]; // Go right 1 -> 2
+    if(y == ymax) return [-1,0]; // Go left 4 -> 5
+
+    return [1,0]; // Go right 8 -> 9
 }
 
-read(args[0], function(data) {
-    var lines = data.split('\n');
-
-    for(let l of lines) {
-        if(l.length == 0) continue;
-        var nums = l.split(/\s+/).map((a) => parseInt(a));
-        
-    }
-
-});
-
+// Calculate distance
+function manh(from, to) {
+    return (Math.abs(from.x - to.x) + Math.abs(from.y - to.y));
+}
