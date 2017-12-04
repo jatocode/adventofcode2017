fe65705dd9eb9c268698cd1b24cbf30f07165bbe
Tobias Jansson <tobias.jansson@gmail.com>
2017-12-04 10:31:35 +0100

    Part 2. Not pretty, but I should be working

--- 4.js
+++ 4.js
@@ -12,7 +12,10 @@ function read(file, callback) {
 
 read(args[0], function(data) {
     var lines = data.split('\n');
-    var numvalid = 0;
+    var numvalid1 = 0;
+    var numvalid2 = 0;
+    var l1 = '';
+    var l2 = '';
     for(let l of lines) {
         if(l.length == 0) continue;
 
@@ -23,12 +26,39 @@ read(args[0], function(data) {
             var match = l.match(reg); 
             if(match.length > 1) {
                 valid = false;
+                l1 = '';
                 break;
             }
             valid = true;
         }
-        if (valid) numvalid++;
+        if (valid) {
+            numvalid1++;
+            l1 = l;
+        }
+
+        // Part 2
+        valid = false;
+        for(p1 of pass) {
+            var matches = 0;
+            var ana1 = p1.split('').sort().join('');
+            for(p2 of pass) {
+                var ana2 = p2.split('').sort().join('');
+                if(ana1 == ana2) {
+                    matches++;
+                }
+            }
+            if(matches > 1) {
+                valid = false;
+                break;
+            }
+            valid = true;
+            
+        }
+        if(valid) {
+            numvalid2++;
+        }
     }
-    console.log('Number of valid ' + numvalid);
+    console.log('Number of valid part 1 ' + numvalid1);
+    console.log('Number of valid part 2 ' + numvalid2);
 });
 
