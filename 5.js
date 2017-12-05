e8c536c8e2a9080f845ec5ba698376092b2f40d8
Tobias Jansson <tobias.jansson@gmail.com>
2017-12-05 20:44:34 +0100

    Dag 5, del 2

--- 5.js
+++ 5.js
@@ -14,7 +14,7 @@ read(args[0], function(data) {
     var maze = data.split('\n').map(x => { return parseInt(x)});
     var current = 0;
     var steps = 1;
-    while(steps < 500000) { // Hänglsen & livrem
+    while(steps < 500000) { // Hängslen & livrem
         var jump = maze[current];
         if(current + jump >= maze.length) break;
         maze[current]++;
@@ -24,5 +24,22 @@ read(args[0], function(data) {
     }
     console.log('Part 1, number of steps ' + steps);
 
+    // Copy paste för del 2
+    var maze = data.split('\n').map(x => { return parseInt(x)});    
+    var current = 0;
+    var steps = 1;
+    while(steps < 100000000) { // Hängslen & livrem
+        var jump = maze[current];
+        if(current + jump >= maze.length) break;
+        if(jump >= 3) {
+            maze[current]--;
+        } else {
+            maze[current]++;
+        }
+        current += jump;
+        
+        steps++;
+    }
+    console.log('Part 2, number of steps ' + steps);
 });
 
