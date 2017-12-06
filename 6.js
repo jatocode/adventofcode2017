147e2516aeaa32ab39f64c78df15293711049b3a
Tobias Jansson <tobias.jansson@gmail.com>
2017-12-06 07:34:17 +0100

    Förberedde för dag 6, bytte lite namn

---
+++ 6.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+var args = process.argv.slice(2);
+
+function read(file, callback) {
+    fs.readFile(file, 'utf8', function(err, data) {
+        if (err) {
+            console.log(err);
+        }
+        callback(data);
+    });
+}
+
+read(args[0], function(data) {
+    var lines = data.split('\n');
+
+});
+
