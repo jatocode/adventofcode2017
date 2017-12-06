a796a731d8393e39188d03198d2a623df7b7f128
Tobias Jansson <tobias.jansson@gmail.com>
2017-12-06 10:48:44 +0100

    Dag 6, del 1

--- 6.js
+++ 6.js
@@ -11,7 +11,31 @@ function read(file, callback) {
 }
 
 read(args[0], function(data) {
-    var lines = data.split('\n');
+    var blocks = data.split(/\s+/).map(x => {return parseInt(x)});
+    var seen = [];
+    var steps = 0;
+    do {
+        seen.push(blocks.toString());
+        steps++;
+        var max = Math.max(...blocks);
+        var maxi = blocks.indexOf(max);
 
-});
+        var dist1 = Math.ceil(max / blocks.length);
+        var dist2 = max % dist1;
+        blocks[maxi] = 0;
+
+        var num = Math.floor(max / dist1);
+
+        for(var i=1; i<=num; i++) {
+            var index = (maxi + i) % blocks.length;
+            blocks[index] += dist1;
+        }
+        var index = (maxi + blocks.length) % blocks.length; 
+        blocks[index] += dist2;
+        console.log(blocks);
+
+        var newSeen = blocks.toString();
+    } while(seen.indexOf(newSeen) < 0);
+    console.log('Found loop at step: ' + steps);
+ });
 
