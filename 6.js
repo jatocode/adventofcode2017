d541a83034b70099331f92955b442ad29579dd53
Tobias Jansson <tobias.jansson@gmail.com>
2017-12-06 11:43:50 +0100

    Dag 6, del 1. Rensade massa onödiga uträkningar. Hjärnsläpp imorse.

--- 6.js
+++ 6.js
@@ -13,6 +13,7 @@ function read(file, callback) {
 read(args[0], function(data) {
     var blocks = data.split(/\s+/).map(x => {return parseInt(x)});
     var seen = [];
+    var newseen = '';
     var steps = 0;
     do {
         seen.push(blocks.toString());
@@ -20,22 +21,17 @@ read(args[0], function(data) {
         var max = Math.max(...blocks);
         var maxi = blocks.indexOf(max);
 
-        var dist1 = Math.ceil(max / blocks.length);
-        var dist2 = max % dist1;
         blocks[maxi] = 0;
-
-        var num = Math.floor(max / dist1);
-
-        for(var i=1; i<=num; i++) {
-            var index = (maxi + i) % blocks.length;
-            blocks[index] += dist1;
+        for(var i=1;i<=max;i++) {
+            blocks[(maxi + i) % blocks.length]++;
         }
-        var index = (maxi + blocks.length) % blocks.length; 
-        blocks[index] += dist2;
-        console.log(blocks);
+        //console.log(blocks);
 
-        var newSeen = blocks.toString();
+        newSeen = blocks.toString();
     } while(seen.indexOf(newSeen) < 0);
-    console.log('Found loop at step: ' + steps);
+
+    console.log('Part 1. Found loop at step: ' + steps);
+
+    console.log('Cycle of already seen is: ' + (seen.length));
  });
 
