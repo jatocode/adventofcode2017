5a7d6cc81a9adb3284f6c1c010261f819dd4c5bb
Tobias Jansson <tobias.jansson@gmail.com>
2017-12-14 14:34:34 +0100

    Dag 13 del 2, optimerad så det inte tar flera timmar. Nu sekunder

--- 13.js
+++ 13.js
@@ -39,55 +39,52 @@ read(args[0], function (data) {
     var sum = 0;
     for (l of layers) {
         if (l) sum += l.caught > 0 ? l.layer * l.range : 0;
+        if (l && l.caught) {
+            var la = l.layer == 0?1:l.layer;
+        }
     }
     console.log('Del 1, severity = ' + sum);
 
     // Part 2
+    // Jag optimerade skiten ur del 1 lösningen, gjorde en lookup för att räkna ut skanner
+    // och tog bort alla loopar
+
+    var scanners = [];
+    // Create scanner lookups
+    for(l of layers) {
+        if (l) {
+            var scanner = [];
+            for(i=0;i<l.range;i++) {
+                scanner.push(i);
+            }
+            for(i=l.range-2;i>=0;i--) {
+                scanner.push(i);
+            }
+            scanners[l.layer] = scanner;
+        }
+    }
+
     var delay = 0;
-    // Reset
     while (true) {
-        for (l of layers) {
-            if (l) {
-                l.scanpos = 0;
-                l.scandir = 1;
-                l.caught = 0;
-            }
-        }
-        // Run until delay
-        var time = 0;
-        for (j = 0; j < delay; j++) {
-            for (i = 0; i < layers.length; i++) {
-                for (l of layers) {
-                    if (l) {
-                        l.scanpos = l.scanpos + l.scandir;
-                        if (l.scanpos == l.range - 1 || l.scanpos == 0) l.scandir *= -1;
-                    }
-                }
-                time++;
-            }
-        }
+        var time = delay;
+        time++;
 
         // Test with this delay
         var caught = false;
         for (i = 0; i < layers.length; i++) {
             var layer = layers[i];
             if (layer) {
-                if (0 == layer.scanpos) {
+                var lid = layer.layer;
+                var pos = (time-1) % (scanners[lid].length-1); 
+                var calc = scanners[lid][pos];
+                if (0 == calc) {
                     caught = true;
                     delay++;
                     break;
                 }
             }
-            for (l of layers) {
-                if (l) {
-                    l.scanpos = l.scanpos + l.scandir;
-                    if (l.scanpos == l.range - 1 || l.scanpos == 0) l.scandir *= -1;
-                }
-            }
             time++;
         }
-        if(delay % 100 == 0) console.log(delay);
-
         if (!caught) break;
     }
     console.log('Del 2, perfekt fördröjning:' + delay);
