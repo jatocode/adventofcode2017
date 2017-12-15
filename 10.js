3ccdf9168975f0b3e8d66e98b914940c915c2d56
Tobias Jansson <tobias.jansson@gmail.com>
2017-12-15 08:53:51 +0100

    Dag 10 del 2. Smygkollade på P. Norvigs lösning pga orkade inte själv

--- 10.js
+++ 10.js
@@ -27,9 +27,72 @@ read(args[0], function(data) {
         skipsize++;
     }
 
-    console.log(list.slice(0,2) + ' = ' + list[0] * list[1]);
+    console.log('Del 1: Totalen är: ' + list.slice(0,2) + ' = ' + list[0] * list[1]);
 
+    // Del 2
+    var input = data.split('\n')[0];
+    console.log('Del 2: Knothash of ' + input + ' = ' + knothash(input, len, 64));
+
+    // Tests
+    //console.log('AoC 2017 = ' + knothash('AoC 2017', 256, 64));
+    //var test = xor([65,27,9,1,4,3,40,50,91,7,6,0,2,5,68,22]);
+    //console.log(test);
 });
+
+function knothash(input, len, rounds) {
+    var lengths = input.split('').map((x) => x.charCodeAt(0));
+    lengths = lengths.concat([17, 31, 73, 47, 23]);
+
+    var list=[];
+    for(i=0;i<len;i++) {
+        list[i] = i;
+    }
+
+    // Kör len varv
+    var currentpos = 0;
+    var skipsize = 0;
+    for(r=0;r<rounds;r++) {
+        for(length of lengths) {
+            list = reverse(list, currentpos, length);
+            currentpos = (currentpos + length + skipsize) % list.length;
+            skipsize++;
+        }
+    }
+
+    // Sparse hash till dense hash
+    var dense = densehash(list, 16);
+
+    return tohex(dense);
+}
+
+function tohex(list) {
+    var hex = '';
+    for(l of list) {
+        // Snodde kod för 0-pad från stackoverflow
+        var d = "00"+l.toString(16);
+        var e = d.substr(d.length-2);
+        hex += e;
+    }
+    return hex;
+}
+
+function densehash(list, size) {
+    var dense = [];
+    for(i=0;i<list.length;i+=size) {
+        block = list.slice(i, i+size);
+        dense.push(xor(block));        
+    }
+    return dense;
+}
+
+function xor(block) {
+    var x = 0;
+    for(b of block) {
+        x = x ^ b;
+    }
+    return x;
+}
+
 
 function reverse(list, pos, num) {
     var safe = list.slice(pos).concat(list.slice(0, pos));
