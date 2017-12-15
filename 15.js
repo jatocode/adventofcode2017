8e31d6a27a7a1695edac4e8c102692fe3a4cee09
Tobias Jansson <tobias.jansson@gmail.com>
2017-12-15 11:09:44 +0100

    Strulade lite med modulo och & men till slut så

--- 15.js
+++ 15.js
@@ -12,8 +12,19 @@ while(pairs-- > 0) {
 
     if((prevA & 0xffff) == (prevB & 0xffff)) judge++;
 
-//    if(pairs % 10000 == 0) console.log(pairs);
+}
+console.log('Part 1. The judge has counted: ' + judge);
+
+// Part 2
+prevA = input[0]; 
+prevB = input[1];
+pairs = 5000000;
+var judge = 0;
+for(i=0;i<pairs;i++) {
+    do { prevA = (prevA * 16807) % 2147483647; } while(prevA % 4 !=0);
+    do { prevB = (prevB * 48271) % 2147483647; } while(prevB % 8 !=0); 
+
+    if((prevA & 0xffff) == (prevB & 0xffff)) judge++;
 
 }
-console.log('The judge has counted: ' + judge);
-
+console.log('Part 2. The judge has counted: ' + judge);
