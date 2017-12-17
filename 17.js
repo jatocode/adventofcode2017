1becdefbf60a6ad91d5e8a94e4e68f6234c5e0f0
Tobias Jansson <tobias.jansson@gmail.com>
2017-12-18 00:38:21 +0100

    Del 2

--- 17.js
+++ 17.js
@@ -5,9 +5,20 @@ var cp = 0;
 var buffer = [cp];
 for(let i=1;i<2018;i++) {
     let index = (cp+steps) % buffer.length + 1;
-    //pb(cp, buffer);
     buffer.splice(index, 0, i);
     cp = index;
 }
-var i2017 = buffer.indexOf(2017);
-console.log(buffer[(i2017 + 1) % buffer.length]);
+var indexAfter = buffer.indexOf(2017);
+console.log('Del 1: ' + buffer[(indexAfter + 1) % buffer.length]);
+
+// Del 2
+cp = 0;
+after = 0;
+for(let i=1;i<=50E6;i++) {
+    let index = (cp+steps) % i + 1;
+    cp = index;
+    if(index == 1) {
+        after = i;
+    }
+}
+console.log('Del 2: ' + after);
