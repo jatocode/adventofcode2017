6ad206ddf20b352cf63184371d648888f8f3f000
Tobias Jansson <tobias.jansson@gmail.com>
2017-12-18 14:36:59 +0100

    Prepared for threading

--- 18.js
+++ 18.js
@@ -10,62 +10,75 @@ function read(file, callback) {
     });
 }
 
-var registers = []
+const RUN = 0;
+const WAIT = 1;
+
 read(args[0], function (data) {
     var instructions = data.split('\n');
     var program = [];
     for (inst of instructions) {
         let match = inst.match(/(\w+)\s+(.*?)(\s+(.*)|$)/);
         if(match) {
-            let ins = {i:match[1], lvalue:match[2].trim(), rvalue:match[3].trim()};
+            let ins = {
+                i:match[1], 
+                lvalue:match[2].trim(), 
+                rvalue:match[3].trim()
+            };
             program.push(ins);
         } 
     }
-    console.log('Last frequency: ' + run(program));
+    console.log('Last frequency: ' + run(0, program));
 });
 
-function run(program) {
+function run(id, program) {
+    var registers = {v: [], 
+        get: function(x) { 
+            if(Object.keys(this.v).indexOf(x) > -1) {
+                return this.v[x];
+            }
+            return parseInt(x);
+        }
+    };
     var ip = 0;
     var freq = 0;
+    var queue = [];
+    var state = RUN;
     while(ip < program.length) {
         let p = program[ip];
         let i = p.i;
         let lvalue = p.lvalue;
         let rvalue = p.rvalue;
+        if(i == 'rcv') {
+            if(registers.get(lvalue) > 0) return freq;
+            //    TODO Check my queue
+            //    var val = queue.shift();
+            //    registers.v[lvalue] = val;
+        }
         switch(i) {
             case 'snd': 
-                freq = get(lvalue); 
+                freq = registers.get(lvalue); 
+                // TODO Send to other [1-id]
                 break;
             case 'set': 
-                registers[lvalue] = get(rvalue);
+                registers.v[lvalue] = registers.get(rvalue);
                 break;
             case 'add': 
-                registers[lvalue] += get(rvalue);
+                registers.v[lvalue] += registers.get(rvalue);
                 break;
             case 'mul': 
-                registers[lvalue] *= get(rvalue);
+                registers.v[lvalue] *= registers.get(rvalue);
                 break;
             case 'mod':
-                registers[lvalue] %= get(rvalue);
-                break;
-            case 'rcv': 
-                if(get(lvalue) > 0) return freq;
+                registers.v[lvalue] %= registers.get(rvalue);
                 break;
             case 'jgz': 
-                if(get(lvalue) > 0) {
-                    ip += get(rvalue);
+                if(registers.get(lvalue) > 0) {
+                    ip += registers.get(rvalue);
                     continue;
                 }
                 break;
         }
         ip++;
     }
-}
-
-function get(x) {
-    if(Object.keys(registers).indexOf(x) > -1) {
-        return registers[x];
-    }
-    return parseInt(x);
 }
 
